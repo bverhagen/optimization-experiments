a15acec714f04986985c05e8e435f896b5df3f35
Bart Verhagen <barrie.verhagen@gmail.com>
2016-02-17 21:31:14 +0100

    Added example on how the benchmark framework works

---
+++ 00-benchmark/benchmark.cpp
@@ -0,0 +1,19 @@
+#include <set>
+#include "benchmark/benchmark.h"
+
+static void BM_StringCreation(benchmark::State& state) {
+  while (state.KeepRunning())
+    std::string empty_string;
+}
+// Register the function as a benchmark
+BENCHMARK(BM_StringCreation);
+
+// Define another benchmark
+static void BM_StringCopy(benchmark::State& state) {
+  std::string x = "hello";
+  while (state.KeepRunning())
+    std::string copy(x);
+}
+BENCHMARK(BM_StringCopy)->Range(8, 8<<10);
+
+BENCHMARK_MAIN();
