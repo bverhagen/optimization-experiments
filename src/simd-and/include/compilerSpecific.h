eb89c3f9792070029de6171f6306e2a42fec18a4
Bart Verhagen <barrie.verhagen@gmail.com>
2016-06-09 22:12:14 +0200

    Changed compiler specific file to have all compiler specific stuff for all compilers into one file.

--- src/simd-and/include/compilerSpecific.h
+++ src/simd-and/include/compilerSpecific.h
@@ -1,15 +1,24 @@
 #ifndef __COMPILERSPECIFIC_H__
 #define __COMPILERSPECIFIC_H__
 
-#include <cstdint>
-
+#if defined(__GNUC__) || defined(__clang__)
 // Provisions for defining how likely it is a take a branch
 #define likely(x)    __builtin_expect (!!(x), 1)
 #define unlikely(x)  __builtin_expect (!!(x), 0)
+#endif
 
 namespace SimdAnd {
+#if defined(__GNUC__)
+	const size_t BIGGEST_ALIGNMENT = __BIGGEST_ALIGNMENT__;
+	typedef std::uint8_t v4uint8_t __attribute__ ((vector_size (__BIGGEST_ALIGNMENT__ / sizeof(std::uint8_t))));	
+
+#elif defined(__clang__)
 	const size_t BIGGEST_ALIGNMENT = 16;            // Hard code to 16 bytes for now
 	typedef std::uint8_t v4uint8_t __attribute__((__aligned__(BIGGEST_ALIGNMENT)));	
+
+#endif
 }
+
+
 
 #endif
