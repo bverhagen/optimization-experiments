6d6d01f15f1314218947da48a4e8d135fa96bd5c
Bart Verhagen <barrie.verhagen@gmail.com>
2016-07-14 20:40:13 +0200

    Fixed python2 issue with the super() command in ValgrindMemcheck.

--- exec_utils/filter/valgrindMemcheck.py
+++ exec_utils/filter/valgrindMemcheck.py
@@ -2,4 +2,4 @@ from .valgrind import *
 
 class ValgrindMemcheck(Valgrind):
     def __init__(self):
-       super(ValgrindMemcheck, self).__init__('memcheck')
+       Valgrind.__init__(self, 'memcheck')
